c807d9c2c0f50f33f44e0d45137e876e6af81096
Lorram Marques <gabriellorranaraujomarques@gmail.com>
2025-03-18 10:49:49 -0300

    Project created

---
+++ backend/src/main/java/com/example/agendator/AgendatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/agendator/AgendatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agendator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
